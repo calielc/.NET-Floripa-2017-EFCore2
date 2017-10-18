29ded6ef39015e7e008d49ecc8b7828106950dd2
caliel <caliel@calielcosta.com>
2017-10-17 23:48:36 -0200

    add web project

---
+++ EFCore.Web/Controllers/PeopleController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using EFCore.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCore.Web.Controllers {
+    [Route("peoples")]
+    public class PeoplesController : Controller {
+        [HttpGet]
+        public IEnumerable<People> List() {
+            using (var context = new MediaContext()) {
+                return context.Peoples.ToArray();
+            }
+        }
+        [HttpGet, Route("includeDeleted")]
+        public IEnumerable<People> ListIncludeDeleted() {
+            using (var context = new MediaContext()) {
+                return context.Peoples.IgnoreQueryFilters().ToArray();
+            }
+        }
+    }
+}
---
+++ EFCore.Web/Controllers/PingController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EFCore.Web.Controllers {
+    [Route("ping")]
+    public class PingController : Controller {
+        // GET api/values
+        [HttpGet]
+        public DateTime Get() {
+            return DateTime.Now;
+        }
+    }
+}
