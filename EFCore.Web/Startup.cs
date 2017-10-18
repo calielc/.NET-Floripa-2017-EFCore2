97feb121e322024b41462171d92a5c84a98fbe93
caliel <caliel@calielcosta.com>
2017-10-18 00:57:50 -0200

    using db pool

--- EFCore.Web/Startup.cs
+++ EFCore.Web/Startup.cs
@@ -16,7 +16,7 @@ namespace EFCore.Web {
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
             var connectionString = Configuration.GetConnectionString("MyConnection");
-            services.AddDbContext<MediaContext>(options => options.UseSqlServer(connectionString));
+            services.AddDbContextPool<MediaContext>(options => options.UseSqlServer(connectionString));
 
             services.AddMvc();
         }
