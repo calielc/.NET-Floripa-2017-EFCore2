776c957c6d33ed2c8836305b7059780c55a320fb
caliel <caliel@calielcosta.com>
2017-10-18 00:47:26 -0200

    Add photo controller

---
+++ EFCore.Web/Controllers/PhotosController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using EFCore.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCore.Web.Controllers {
+    [Route("photos")]
+    public class PhotosController : Controller {
+        private readonly MediaContext _context;
+
+        public PhotosController(MediaContext context) {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Photo> List() {
+            return _context.Photos.Include(x => x.Photographer).ToArray();
+        }
+    }
+}
