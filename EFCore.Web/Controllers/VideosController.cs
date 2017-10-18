c61d962a4592f9b05f3fe1fe5ac5157ce23f03d8
caliel <caliel@calielcosta.com>
2017-10-18 01:07:20 -0200

    interpolação de strings

---
+++ EFCore.Web/Controllers/VideosController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using EFCore.Data;
+using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCore.Web.Controllers {
+    [Route("videos")]
+    public class VideosController : Controller {
+        private readonly MediaContext _context;
+
+        public VideosController(MediaContext context) {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Video> List() {
+            return _context.Videos.ToArray();
+        }
+
+        [HttpGet, Route("duration")]
+        public IEnumerable<Video> ListWith(TimeSpan greather) {
+            return _context.Videos.FromSql($"select * from Videos where duration > {greather}").ToArray();
+        }
+
+    }
+}
